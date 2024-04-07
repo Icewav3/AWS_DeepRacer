787f43a59ee6e1c837128b21b2a87568390f8375
Avish-Nagiana <155268839+Avish-Nagiana@users.noreply.github.com>
2024-04-07 02:32:47 -0500

    Update AWS_Deeprace_RewardFunc.py

--- AWS_Deeprace_RewardFunc.py
+++ AWS_Deeprace_RewardFunc.py
@@ -89,17 +89,6 @@ def reward_function(params) :
     if abs_steering > ABS_STEERING_THRESHOLD:
         reward *= 0.8
 
-
-    # Steps 
-
-    # Total num of steps we want the car to finish the lap, it will vary depends on the track length
-    TOTAL_NUM_STEPS = 300
-
-    # Give additional reward if the car pass every 100 steps faster than expected
-    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
-        reward += 10.0
-
-
     # Track width
 
     # Calculate the distance from each border
