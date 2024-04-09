452e661696efbfee4d57f9302665f91d0857343f
Icewaveeee <coledorman12@gmail.com>
2024-04-09 13:42:12 -0500

    Update AWS_Deeprace_RewardFunc.py

--- AWS_Deeprace_RewardFunc.py
+++ AWS_Deeprace_RewardFunc.py
@@ -1,14 +1,12 @@
-
 def reward_function(params) :
-    reward = 0
     all_wheels_on_track = params['all_wheels_on_track']
     speed = params['speed']
     x = params['x']
     y = params['y']
+    progress = params['progress']
     is_left_of_center = params['is_left_of_center']
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
-    crashed = params['is_crashed']
     reverse = params['is_reversed']
     off_track = params['is_offtrack']
     heading = params['heading']
@@ -16,10 +14,19 @@ def reward_function(params) :
     track_length = params['track_length']
     distance_from_center = params['distance_from_center']
     steps = params['steps']
-    progress = params['progress']
     steering_angle = params['steering_angle']
-    
-    
-    reward += 1
 
+    if init == False: #update only once
+        center_track = track_width/2
+        init = True
+    reward = 0 #reset
+    reward += all_wheels_on_track
+    reward -= off_track
+    reward -= distance_from_center
+    reward -= reverse
+    reward += progress * 100 #as track completes increase reward - could scale by large amount to make car speed up?
+    reward += speed > 1 #prevent it from not moving and learning
+    
+    
+    
     return float(reward)
